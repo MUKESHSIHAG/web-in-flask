2e3d1cabcc2a2529b2300b8256b0b2f5bedb2899
= <=>
2018-08-28 21:49:58 +0530

    web-in-flask

--- flask-web.py
+++ flask-web.py
@@ -1,16 +1,18 @@
+import feedparser
 from flask import Flask
 app = Flask(__name__)
-
+BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 @app.route("/")
-@app.route("/home")
-def home():
-    return "<h1>Home Page</h1>"
-
-@app.route("/about")
-def about():
-    return "<h1>About Page</h1>"
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-
+def get_news():
+    feed = feedparser.parse(BBC_FEED)
+    first_article = feed['entries'][0]
+    return """<html>
+        <body>
+            <h1> BBC Headlines </h1>
+            <b>{0}</b> <br/>
+            <i>{1}</i> <br/>
+            <p>{2}</p> <br/>
+        </body>
+</html>""".format(first_article.get("title"), first_article. get("published"), first_article.get("summary"))
+if __name__ == "__main__":
+    app.run(port=500, debug=True)
